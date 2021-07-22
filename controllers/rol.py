2f23497fd5b36ba71e657a2a2aad6893fdf2549e
Diego Ustariz <diusme@gmail.com>
2021-07-22 17:36:39 -0500

    DU: Se hace arreglo rol.py de acuerdo al pep8

--- controllers/rol.py
+++ controllers/rol.py
@@ -4,26 +4,31 @@ from controllers.db_session import DBSession
 session = DBSession()
 session = session.get_session()
 
+
 def show_roles_tb():
     roles = session.query(Rol).all()
     return roles
 
+
 def get_rol_by_id(id):
-    rol = session.query(Rol).filter_by(id = id).one()
+    rol = session.query(Rol).filter_by(id=id).one()
     return rol
 
+
 def new_rol_tb(name, description, state):
-    new_rol = Rol(name = name, description = description, state = state)
+    new_rol = Rol(name=name, description=description, state=state)
     session.add(new_rol)
     session.commit()
     session.close()
     return True
+
 
 def edit_rol_tb(rol):
     session.add(rol)
     session.commit()
     session.close()
     return True
+
 
 def delete_rol_tb(id):
     rol = get_rol_by_id(id)
